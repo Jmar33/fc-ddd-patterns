211b57f8924d96c640b02defd87bbc7567799a10
Jmar33 <josimarferreira922@yahoo.com.br>
2025-03-03 09:42:42 -0300

    feat: creating customer events and handlers

---
+++ src/domain/customer/event/handler/print-console.log-when-customer-is-created-2.handler.ts
@@ -0,0 +1,9 @@
+import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
+import CustomerCreatedEvent from "../customer-created.event";
+
+export default class PrintConsoleLogWhenCustomerIsCreatedHandler implements EventHandlerInterface<CustomerCreatedEvent>{
+  handle(event: CustomerCreatedEvent): void{
+    const eventName = event.constructor.name;
+    console.log(`Esse é o segundo console log do evento: ${eventName}`);
+  }
+}
